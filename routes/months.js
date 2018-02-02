3d4b5cc4976998829a5a947d8113e032db51adbb
johntzan <john.tzanida@gmail.com>
2018-02-02 17:49:38 -0500

    add february 2018 to months

--- routes/months.js
+++ routes/months.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET MONTHS */
 router.get('/', function(req, res, next) {
   var monthsAvailable = ["October 2016", "November 2016", "December 2016", "January 2017", "February 2017", "March 2017",
-															"April 2017", "May 2017", "June 2017", "July 2017", "August 2017", "October 2017", "November 2017", "December 2017", "January 2018"];
+															"April 2017", "May 2017", "June 2017", "July 2017", "August 2017", "October 2017", "November 2017", "December 2017", "January 2018", "February 2018"];
 
 			res.json(monthsAvailable);
 });
