90d6c5192efddd78b53a57f23bc567fefcc5250f
John Tzanidakis <john.tzanida@gmail.com>
2018-11-19 05:50:51 -0500

    update months

--- routes/months.js
+++ routes/months.js
@@ -4,17 +4,6 @@ var router = express.Router();
 /* GET MONTHS */
 router.get("/", function(req, res, next) {
   var monthsAvailable = [
-    "October 2016",
-    "November 2016",
-    "December 2016",
-    "January 2017",
-    "February 2017",
-    "March 2017",
-    "April 2017",
-    "May 2017",
-    "June 2017",
-    "July 2017",
-    "August 2017",
     "October 2017",
     "November 2017",
     "December 2017",
@@ -25,7 +14,9 @@ router.get("/", function(req, res, next) {
     "May 2018",
     "June 2018",
     "July 2018",
-    "August 2018"
+    "August 2018", 
+    "October 2018",
+    "November 2018"
   ];
 
   res.json(monthsAvailable);
